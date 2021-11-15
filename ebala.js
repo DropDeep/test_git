c2cace0e43aed3f863c6ca3432d8906d5251a60d
DropDeep <dudarikm@gmail.com>
2021-11-15 18:00:12 +0200

    HTML changes

--- ebala.js
+++ ebala.js
@@ -1,10 +1,36 @@
+
 $(window).on('load', function() {
     $(".Slider").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 2000,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        arrows: true
     });
 });
+
+// let test = document.getComputedStyle(document.querySelector('.slick-prev'), ':before').style.setProperty('content','test')
+
+// window.getComputedStyle(
+//     document.querySelector('slick-prev'), ':before'
+// ).style.setProperty('content', 'test')
+
+// let anotherOne = document.querySelector('root').style.setProperty('--content-left', 'something')
+
+
+// let element = document.querySelector('.slick-prev');
+// let styles = window.getComputedStyle(element,':before')
+// let content = styles['content'].style.setProperty('content', 'obnoenbepir');
+
+
+
+let lastOne = document.querySelector('.Slider');
+lastOne.style.setProperty('--content-left', 'test');
+
+
+
+
+
+
